a2b48b6d1d74abe6ee770f37dbe41786616a4978
attila5287 <atiturkoz@hotmail.com>
2019-01-30 21:24:07 -0700

    app again for mongo
    none

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def lister():
     else:
         mongo_uri = 'mongodb://heroku_m3j2ckrz:3dnq36e1958luohj93avjj3l8s@ds113495.mlab.com:13495/heroku_m3j2ckrz'
         flask_debug = 'false'
-
+    mongo = PyMongo(app,mongo_uri)
     db = client.heroku_m3j2ckrz
     # Run scraper functions to fill up above db in mongo
     fulljobVizdata = JobVizzY.scrapListFrameDict(
